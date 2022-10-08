e628930aa705400f395750e484fbd771f5411d47
danielashrafk <danielashrafk@gmail.com>
2022-10-08 21:19:05 +0200

    Final Commit

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from "@nestjs/common";
 import { Product } from "./product.model";
 import { ProductsService } from "./products.service";
 
@@ -27,6 +35,28 @@ export class ProductsController {
 
   @Get(":id")
   getProduct(@Param("id") prodId: string): { product: Product } {
-    return { product: this.productsService.getProduct() };
+    return { product: this.productsService.getProduct(prodId) };
+  }
+
+  @Patch(":id")
+  updateProduct(
+    @Param("id") prodId: string,
+    @Body("title") title: string,
+    @Body("description") desc: string,
+    @Body("price") price: number
+  ): { updatedProduct: Product } {
+    return {
+      updatedProduct: this.productsService.updateProduct(
+        prodId,
+        title,
+        desc,
+        price
+      ),
+    };
+  }
+
+  @Delete(":id")
+  deleteProduct(@Param("id") prodId: string): { id: string } {
+    return { id: this.productsService.deleteProduct(prodId) };
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Product } from "./product.model";
 
 const { v1: uuidv1 } = require("uuid");
@@ -6,6 +6,13 @@ const { v1: uuidv1 } = require("uuid");
 @Injectable()
 export class ProductsService {
   private products: Product[] = [];
+
+  findProduct(prodId: string): [Product, number] {
+    const prodIndex = this.products.findIndex((prod) => prod.id === prodId);
+    const product = this.products[prodIndex];
+    if (!product) throw new NotFoundException("Could not find product");
+    return [product, prodIndex];
+  }
 
   addProduct(title: string, description: string, price: number) {
     const prodId = uuidv1();
@@ -18,6 +25,26 @@ export class ProductsService {
     return [...this.products];
   }
 
-  getProduct(prodId: string) {}
+  getProduct(prodId: string): Product {
+    const [product, prodIndex] = this.findProduct(prodId);
+    return { ...product };
+  }
+
+  updateProduct(prodId: string, title: string, desc: string, price: number) {
+    const [product, prodIndex] = this.findProduct(prodId);
+
+    if (title) product.title = title;
+    if (desc) product.description = desc;
+    if (price) product.price = price;
+
+    this.products[prodId] = { ...product };
+
+    return { ...this.products[prodId] };
+  }
+
+  deleteProduct(prodId: string) {
+    const [product, prodIndex] = this.findProduct(prodId);
+    this.products.splice(prodIndex, 1);
+    return prodId;
+  }
 }
-    
